147466a25decb92a355aa7d66212a1b48c8023ff
0xB0C5 <theboxxitg@gmail.com>
2020-03-23 22:14:30 -0700

    Add input override.

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace dirt
             string programPath = args[0];
 
             bool verbose = false;
+            byte[] input = null;
 
             for (int i = 1; i < args.Length; i++)
             {
@@ -25,6 +26,18 @@ namespace dirt
                 {
                     case "-v":
                         verbose = true;
+                        break;
+
+                    case "-i":
+                        i++;
+                        if (i < args.Length)
+                        {
+                            input = args[i].Select(c => (byte)c).ToArray();
+                        }
+                        else
+                        {
+                            input = new byte[0];
+                        }
                         break;
 
                     default:
@@ -39,23 +52,26 @@ namespace dirt
             var tree = new Parser(code).Parse();
             var transducer = Transducer.FromSyntaxTree(tree);
 
-            var inStream = Console.OpenStandardInput();
-            var outStream = Console.OpenStandardOutput();
-
-            var buffer = new byte[1024];
-
-            var inputList = new List<byte>();
-
-            while (true)
+            if (input == null)
             {
-                int readCount = inStream.Read(buffer, 0, buffer.Length);
-                if (readCount == 0) break;
+                var inStream = Console.OpenStandardInput();
 
-                inputList.AddRange(buffer.Take(readCount));
+                var buffer = new byte[1024];
+
+                var inputList = new List<byte>();
+
+                while (true)
+                {
+                    int readCount = inStream.Read(buffer, 0, buffer.Length);
+                    if (readCount == 0) break;
+
+                    inputList.AddRange(buffer.Take(readCount));
+                }
+
+                input = inputList.ToArray();
             }
 
-            var input = inputList.ToArray();
-
+            var outStream = Console.OpenStandardOutput();
             while (true)
             {
                 var output = transducer.GetMinimumOutput(input);
